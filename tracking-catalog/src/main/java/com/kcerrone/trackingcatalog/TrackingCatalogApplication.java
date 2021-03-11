4d75563ea452dac03baca38f26097fdd521ba359
Karen Cerrone <kacerrone@gmail.com>
2021-03-10 23:53:51 -0300

    initial commit

---
+++ tracking-catalog/src/main/java/com/kcerrone/trackingcatalog/TrackingCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.kcerrone.trackingcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingCatalogApplication.class, args);
+	}
+
+}
---
+++ tracking-catalog/src/test/java/com/kcerrone/trackingcatalog/TrackingCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcerrone.trackingcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
